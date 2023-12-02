ff5b04ba1dd9dbc7ead6a719508a5f5769586221
Elle Meredith <ellemeredith@gmail.com>
2023-12-03 08:28:33 +1100

    Add pending spec for practice

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,4 +5,21 @@ RSpec.describe Post do
 
   context "validations"
   context "associations"
+
+  describe ".by_person" do
+    it "returns returns posts by a person" do
+      pending
+
+      person = create(:person, first_name: "expected")
+      other_person = create(:person, first_name: "other")
+
+      expected_post_1 = create(:post, person: person, title: "expected-one")
+      expected_post_2 = create(:post, person: person, title: "expected-two")
+      _other = create(:post, person: other_person, title: "other")
+
+      result = Post.by_person(person.id)
+
+      expect(result).to match_array([expected_post_1, expected_post_2])
+    end
+  end
 end
