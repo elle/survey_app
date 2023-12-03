e4cec12c8f662597ab674742262d957891effc5d
Elle Meredith <ellemeredith@gmail.com>
2023-12-03 15:10:48 +1000

    Prep specs for callbacks activity

--- spec/models/survey_spec.rb
+++ spec/models/survey_spec.rb
@@ -5,6 +5,12 @@ RSpec.describe Survey do
     it { is_expected.to belong_to(:company) }
   end
 
+  # context "validations" do
+  #   subject { build(:survey) }
+
+  #   it { is_expected.to validate_uniqueness_of(:slug).case_insensitive }
+  # end
+
   it "is active by default" do
     expect(described_class.new).to be_active
   end
@@ -21,4 +27,26 @@ RSpec.describe Survey do
       expect(result).to match_array([active_survey_1, active_survey_2])
     end
   end
+
+  # describe "#generate_slug" do
+  #   context "when provided manually" do
+  #     it "parameterise user's unput" do
+  #       survey = create(
+  #         :survey,
+  #         name: "My Callback",
+  #         slug: "Callbacks are good sometimes",
+  #       )
+
+  #       expect(survey.slug).to eq "callbacks-are-good-sometimes"
+  #     end
+  #   end
+
+  #   context "when not provided" do
+  #     it "parameterises survey's name" do
+  #       survey = create(:survey, name: "My Callbacks")
+
+  #       expect(survey.slug).to eq "my-callbacks"
+  #     end
+  #   end
+  # end
 end
